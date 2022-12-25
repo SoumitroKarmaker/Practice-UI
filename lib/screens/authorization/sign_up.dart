f3501897aeb6f0bae8769e8339182bec8d6be9d1
soumitrokarmaker <soumitrokarmaker@gmail.com>
2022-12-25 23:56:59 +0600

    Sign Up

--- lib/screens/authorization/sign_up.dart
+++ lib/screens/authorization/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 class Sign_Up extends StatelessWidget {
   const Sign_Up({Key? key}) : super(key: key);
@@ -23,12 +24,12 @@ class Sign_Up extends StatelessWidget {
             Center(
               child: Container(
                 decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.6),blurRadius: 5,spreadRadius: 7
-
-                    )
-                  ],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.6),
+                          blurRadius: 5,
+                          spreadRadius: 7)
+                    ],
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.white),
                 height: MediaQuery.of(context).size.height / 1.3,
@@ -37,13 +38,78 @@ class Sign_Up extends StatelessWidget {
                   padding: const EdgeInsets.all(15.0),
                   child: Column(
                     children: [
-                      Text('Sign Up', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),)
+                      Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AppTextField(
+                        textFieldType: TextFieldType.NAME,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          label: Text('Frist Name'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AppTextField(
+                        textFieldType: TextFieldType.NAME,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          label: Text('Last Name'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AppTextField(
+                        textFieldType: TextFieldType.NAME,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          label: Text('E-mail'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AppTextField(
+                        textFieldType: TextFieldType.NAME,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          label: Text('Password'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      AppTextField(
+                        textFieldType: TextFieldType.NAME,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          label: Text('Confirm Password'),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        width: MediaQuery.of(context).size.width,
+                        height: 40,
+                        child: Center(child: Text('Sign Up')),
+                      ),
+
                     ],
                   ),
                 ),
               ),
             ),
-
           ],
         ),
       ),
